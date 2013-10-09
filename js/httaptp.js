b3ced8c97addae71b3f0e7820516b72e955d9839
Matthew Astley <mca@sanger.ac.uk>
2013-10-09 17:05:40 +0100

    enough UI to show/reload/cancel
    stash (selector, results, jqXHR) on the element
    add buttons to div.control
    define actions for the buttons
    split tap_load_parse out of fetch_tap, to enable Reload
    remove some of the ugly from the styling

--- js/httaptp.js
+++ js/httaptp.js
@@ -13,19 +13,56 @@ function($,        tp) {
     ele.removeClass('loading');
     ele.addClass( results["ok"] ? 'pass' : 'fail' );
   };
+
+  var control_fns = {
+    Show: function(ele, ev) {
+      var showing = ele.hasClass("show");
+      $( ele.prop('selector') ).removeClass("show"); // unshow all
+      if (!showing) {
+        ele.addClass("show");
+      }
+    },
+    Log: function(ele, ev) {
+      console.dir({ url: ele.attr("data-tap-src"),
+                    results: ele.prop("results") });
+    },
+    Cancel: function(ele, ev) {
+      ele.prop('jqXHR').abort();
+    },
+    Reload: function(ele, ev) {
+      ele.prop('jqXHR').abort();
+      tap_load_parse(ele);
+    },
   };
 
-  var fetch_tap = function(ele) {
-    var parser = tp();
-    parser.on("results", function (results) {
-      put_results(results, ele);
-    });
+  var add_controls = function(ele, btns) {
+    ele[0].innerHTML = "<pre></pre><div class='control'></div>";
+    var ctrl = ele.find("div.control")[0];
+    for(var i=0; i<btns.length; i++) {
+      ctrl.innerHTML += "<button data-op='" + btns[i] + "' type='button'>"
+        + btns[i] + "</button>\n";
+    }
+    ele.find("div.control button").on("click", { ele: ele }, control_ev);
+  };
 
-    ele[0].innerHTML = "<pre> Loading ... </pre><div class='control'>\
-  <button> Cancel </button>\
-  <button> Show </button>\
-</div>";
+  var control_ev = function(ev){
+    var op = this.firstChild.textContent;
+    var fn = control_fns[op];
+    var ele = ev.data.ele;
+    if (fn) {
+      fn(ele, ev);
+    } else { // no code for this button (oops)
+      $(this).attr("disabled", "disabled");
+    }
+    return false;
+  };
+
+  var tap_load_parse = function(ele) {
     ele.addClass('loading');
+    ele.find("pre")[0].innerHTML = ' Loading ... ';
+    ele.removeClass('pass');
+    ele.removeClass('fail');
+    ele.prop({ results: null });
 
     var url = ele.attr("data-tap-src");
 
@@ -37,6 +74,13 @@ function($,        tp) {
 
     var fn_deliver = function(data) {
       ele.find("pre")[0].textContent = data; // HTML is quoted for us
+
+      var parser = tp();
+      parser.on("results", function (results) {
+        put_results(results, ele);
+        ele.prop('results', results);
+      });
+
       parser.write(data);
       parser.end();
     };
@@ -47,7 +91,7 @@ function($,        tp) {
       fn_deliver(bogo_doc);
     };
 
-    $.ajax({
+    ele.prop('jqXHR', $.ajax({
       url: url,
       type: "GET",
       dataType: "text",
@@ -55,13 +99,19 @@ function($,        tp) {
       headers: hdrs,
       success: fn_deliver,
       error: fn_error,
-    });
+    }));
+  };
+
+  var fetch_tap = function(got_with, ele) {
+    ele.prop('selector', got_with);
+    add_controls(ele, [ 'Show', 'Cancel', 'Reload', 'Log' ]);
+    tap_load_parse(ele);
   };
 
   var load_to_doc = function(selector) {
     var sel_all = $( selector );
     for (var i=0; i < sel_all.length; i++) {
-      fetch_tap( sel_all.eq(i) );
+      fetch_tap(selector, sel_all.eq(i));
     }
   };
 
@@ -69,5 +119,6 @@ function($,        tp) {
     load_to_doc: load_to_doc,
     fetch_tap: fetch_tap,
     version: version,
+    ctrl: control_fns,
   };
 });
